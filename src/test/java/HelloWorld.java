f7c37b7d41b62005dd8e08b64a4e6f00e3bd0c1e
Rosani26 <94964550+Rosani26@users.noreply.github.com>
2021-11-24 13:48:01 +0100

    Test wie system.out/ln funktioniert
    Test wie system.out/ln funktioniert

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld
+{
+    public static void main ( String[] args)
+    {
+        System.out.print("Hello");
+        System.out.print("World");
+        System.out.println("---------");
+
+    }
+}
